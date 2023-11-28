4c7872defe180910fcfd074910bf05cda6de56dc
isoh <ssoboro1@Gmail.com>
2023-11-28 22:36:54 +0900

    add - 학습한내용 추가

--- src/test/java/com/example/demo/apply/InitTransactionTest.java
+++ src/test/java/com/example/demo/apply/InitTransactionTest.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.apply;
 
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
--- src/test/java/com/example/demo/apply/TxBasicTest.java
+++ src/test/java/com/example/demo/apply/TxBasicTest.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.apply;
 
 
 import org.springframework.boot.test.context.SpringBootTest;
